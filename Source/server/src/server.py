a87f5850e1e34338205310b8f8c67678e6606234
tienthanh214 <39771202+tienthanh214@users.noreply.github.com>
2021-05-14 00:51:26 +0700

    update SERVER GUI

--- Source/server/src/server.py
+++ Source/server/src/server.py
@@ -1,4 +1,5 @@
 import sys
+from tkinter.constants import BOTTOM, LEFT, NSEW, RIGHT, TOP
 sys.path.insert(0, '../../utility')
 import tkinter as tk
 from mysocket import MySocket
@@ -6,21 +7,28 @@ from mysocket import MySocket
 class Server:
     def __init__(self):
         super().__init__()
+        # Initialize socket
+        self.IP = ("127.0.0.1", 54321)
         # Initialize GUI
         self._root = tk.Tk()
-        self._root.geometry("1000x900")
+        self._root.geometry("1000x800")
         self._root.title("Library Server")
         self._root.lbl_title = tk.Label(self._root, text = "ONLINE LIBRARY SERVER", width = 25,
                                             font = ("Consolas 30 bold"), fg = "#ff0000")
+        self._root.lbl_address = tk.Label(self._root, text = "Address: " + str(self.IP[0]), width = 25,
+                                            font = ("Consolas 25 bold"))
+        self._root.lbl_logs = tk.Label(self._root, text = "Message:", width = 25,
+                                            font = ("Consolas 15 bold")
+        )
+        self._root.btn_disconnect = tk.Button(self._root, text = "DISCONNECT", width = 12, 
+                                            font = "Consolas 20 bold")
+        
         self._root.logs = tk.Text(self._root, width = 115, height = 40)
-        # self._root.logs.grid(row = 1, column = 0, sticky = tk.N, padx = 10, pady = 10, columnspan = 3)
-        # self._root.logs.place(relx = 0.5, rely = 0.65, anchor = tk.CENTER)
-        # self._root.lbl_title.grid(row = 0, column = 0, sticky = tk.N)
-        # self._root.logs.grid(column = 0, row = 0, sticky = "EW")
-        self._root.lbl_title.grid(column = 0, row = 0)
-        self._root.lbl_title.grid(row = 1, column = 0)
-
-        #self._root.logs.place(x = 50, y = 100)
+        self._root.lbl_title.pack()
+        self._root.lbl_address.pack(pady = (5, 25))
+        self._root.lbl_logs.pack(side = TOP, anchor = "w")
+        self._root.btn_disconnect.pack(side = BOTTOM, anchor = "e", pady = 30, padx = 50)
+        self._root.logs.pack(side = BOTTOM)
         
     def runApplication(self):
         self._root.mainloop()
